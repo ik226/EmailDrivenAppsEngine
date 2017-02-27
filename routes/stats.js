3262953d1c4cafaada974f0d2a764d005bd9d72d
ik226 <ik226@cornell.edu>
2017-02-27 14:49:47 -0500

    Update stats.js
    added a condition when the user's data has not been stored before (i.e. the first visit)

--- routes/stats.js
+++ routes/stats.js
@@ -22,24 +22,30 @@ module.exports = function (req, res) {
 		}
 		mongoDbApi.getUser(email, function (err, user) {
 			if (err) res.send(500, 'error:stats.js:getUser:01');
-			// make token object to request new access token
-			var newToken ={}
-			newToken.access_token = user[0].access_token;
-			newToken.refresh_token = user[0].refresh_token;
+			
+			//if the user's data has not been stored before(i.e. first visit)
+			if(user.length==0){ res.send(200, JSON.stringify(user, null, "  ")); }
+			else{
+				// make token object to request new access token
+				var newToken ={}
+				newToken.access_token = user[0].access_token;
+				newToken.refresh_token = user[0].refresh_token;
 					
-			//request new access token 
-			oauth2.genNewAccessToken(newToken, function(err, tokens){
-				var accessToken = tokens.access_token;
-				xoauth2_token = oauth2.buildXoauth2Token(email, accessToken);
-				//request imap to get emails
-				imap.getEmails(xoauth2_token, email, function(result){
-					//console.log(result);
+				//request new access token 
+				oauth2.genNewAccessToken(newToken, function(err, tokens){
+					var accessToken = tokens.access_token;
+					xoauth2_token = oauth2.buildXoauth2Token(email, accessToken);
+					//request imap to get emails
+					imap.getEmails(xoauth2_token, email, function(result){
+						//console.log(result);
 					
-				}); 
-				//console.log(tokens);
-			});
+					}); 
+					//console.log(tokens);
+				
+				});
 					
-      		res.send(200, JSON.stringify(user, null, "  "));
+      				res.send(200, JSON.stringify(user, null, "  "));
+			}
 		});
 	return;
 	}
