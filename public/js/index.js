7bf50ae689db66b16a124e33781846e5a38f4e20
Il Kwon <ik226@cornell.edu>
2017-03-22 02:15:07 -0400

    updated getJson: make async true

--- public/js/index.js
+++ public/js/index.js
@@ -1,98 +1,121 @@
+
 $( document ).ready(function() {
   
 console.log('Entered Page');
 console.log('loading cookie val: ', getCookie('loading'));
+console.log(document.cookie.split(';'));
+
 
 // If loading cookie set,then wait for email fetch complete.
+
+//why do we need this part?
+/*
+if(getCookie('loading') != ''){
+	console.log('cookie loaidng is none')
+}
+*/
+
 if (getCookie('loading') != '') {
 	$('#loadingDiv').show();
 	console.log('we will start after loading complete');
 	var email = getCookie('email');
-  $('#loadingDivEmail').html (decodeURIComponent(email));
+  $('#loadingDivEmail').html(email);
 	console.log('email: ', email);
 	var timer=0;
   //load status
 	function startTime() {
 		userDataLoaded(email, function (e) {
 			console.log('curr load status: ', e);
+			//TODO: it is never ending
+			// add condition?
 			if (e == 0) {
-				console.log('pending')
-                $('#container').slideDown();
-        			timer =timer+5;
+				if (timer == 100){
+					alert('please revisit');
+					return
+				}
+				console.log('pending');
+      		  	timer =timer+5;
 				$('#loadingDivProgress').html(timer+'%');
-				t = setTimeout(function () {
-						startTime()
-					}, 2000);
+				
+				// change setTimeout to setInterval
+				// setTimeout: calls a function(startTime) only once after 2000 millisecs
+				// setInterval: calls startTime() every 2000 milliseconds
+				var t = window.setTimeout(function(){ startTime(); }, 2000);
+				//var t = window.setInterval(function(){ startTime() }, 2000);
 			} else if (e == -1) {
 				console.log('errpor 11');
-        			alert('error 11. Email not in DB. Refresh Page');
+        		alert('error 11. Email not in DB. Refresh Page');
 			} else if (e == 1) {
-				console.log('loaded')
+				console.log('loaded');
 				setCookie('loading', 0, 0);
 				setCookie('email', 0, 0);
-				setCookie('email', email, 1);
-        			$('#loadingDivMsg').slideUp();
-        			$('#existUserDivEmail').html(decodeURIComponent(email)); // decode "%40" to "@"
+        		setCookie('email', email, 1);
+        		$('#loadingDivMsg').slideUp();
+        		$('#existUserDivEmail').html(decodeURIComponent(email));
 				$('#existUserDiv').slideDown();
-                $('#container').slideDown();
         
 			} else {
-				consoole.log('error10')
-        			alert('error 10. Unknown. Refresh Page');
+				console.log('error10')
+        		alert('error 10. Unknown. Refresh Page');
 			}
 		})
 	}
 	startTime();
 }
+
 //else, normal visit
 else {
-	console.log('normal visit');
-	$("#newUserDiv").show();
 
-	$("#newUserDivSubmit").click(function () {
-		var email = $('#newUserDivEmail').val();
+	console.log('normal visit');
+	$("#emailDiv").show();
+
+	$("#emailDivPlay").click(function () {
+		var email = $('#emailDivEmail').val();
 		if (!validateEmail(email)) {
-			$('#newUserDivEmail').val('Invalid Email')
+			$('#emailDivEmail').val('invalid email')
 			return 0;
 		}
 		userDataExists(email, function (exists) {
 			//email data not exist
+			console.log(exists);
 			if (!exists) {
 				console.log('no data for email');
-				/***$('#emailDiv').slideUp();
+				$('#emailDiv').slideUp();
 				$('#newUserDiv').slideDown();
-				$('#newUserDivEmail').val(email);***/
-				//$("#newUserDivSubmit").click(function () {
-				console.log('yo');
-				var serial = $('#newUserDivForm').serialize();
-				console.log('form serial:', serial);
-				var url = "/login?" + serial;
-				window.location.assign(url);
-            //});
-                return 0;
+				$('#newUserDivEmail').val(email);
+				$("#newUserDivSubmit").click(function () {
+					
+					var serial = $('#newUserDivForm').serialize();
+					console.log('form serial:', serial);
+					var url = "/login?" + serial;
+					
+					window.location.assign(url);
+					//window.location.assign('/spacegame/index_o.html'); //attempt1
+				});
+				return 0;
 			}
 			//email data exists
 			else {
 				console.log('yes data for email');
-                //$("#newUserDivSubmit").click(function () {
-                console.log('yo');
-				var serial = $('#newUserDivForm').serialize();
-				console.log('form serial:', serial);
-				var url = "/login?" + serial;
-				window.location.assign(url);
-				//});
-				return 0;
-				//$('#newUserDiv').slideUp();
-        			$('#existUserDivEmail').html(decodeURIComponent(email)); // decode "%40" to "@"
-        			setCookie('email', email, 1);
+				$('#emailDiv').slideUp();
+				
+				//TODO: add encoder to get @ instead of %40
+				//fixed
+        		$('#existUserDivEmail').html(
+						decodeURIComponent(email));
+				
+        		setCookie('email', email, 1);
 				$('#existUserDiv').slideDown();
+				
+		
+				
 				return 1;
 			}
 		});
 
 	});
 }
- 
+
 $('#existUserDivStart').click(function(){
   window.location.assign('/spacegame');
 })
@@ -134,10 +157,13 @@ function userDataLoaded(email, callback) {
 	})
 };
 
+
+
 //returns 0 or 1; -1 on error
 function userDataExists(email, callback) {
 	//check if user data exists
-	var url = "/stats.json?id=getUser&email=" + email;
+	
+	var url = "/stats.json?id=getUser&email=" + email; 
 	getJson(url, function (err, json) {
 		if (err) {
 			console.log('errorrr');
@@ -149,22 +175,25 @@ function userDataExists(email, callback) {
 				callback(0);
 				return 0; //no data
 			} else {
-				console.log(1, json);
+				//console.log(1, json);
 				callback(1);
 				return 1; //yes data
 			}
 		}
 	})
 };
-  
+
+
 /*
 * Util Functions
 */  
-  
+
+
  function getJson(url, callback) {
  	//open url with GET. Convert response to JSON n return it.
  	//callback(err, json)
- 	xmlhttp = new XMLHttpRequest();
+ 	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
  	xmlhttp.onreadystatechange = function () {
  		if (xmlhttp.readyState == 0) {}
  		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -172,15 +201,22 @@ function userDataExists(email, callback) {
  			var res = xmlhttp.responseText;
  			var resJson = JSON.parse(res);
  			callback(null, resJson);
+		
  		} else if (xmlhttp.readyState == 4) {
  			//fail
  			callback('error loading json ' + url);
  		}
+			
  	}
- 	xmlhttp.open("GET", url, false);
+	
+ 	
  	xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
  	xmlhttp.send();
+	//var res = xmlhttp.responseText;
+	//var resJson = JSON.parse(res);
+	//callback(null, resJson);
  };
+ 
  
  function validateEmail(email) {
  	var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
@@ -215,4 +251,14 @@ function userDataExists(email, callback) {
  		//console.log(cname + "=" + cvalue + "; " + domain + expires)
  		document.cookie = cname + "=" + cvalue + "; " + domain + expires;
  }
-
+ 
+ 
+ /*
+ var popit = true;
+ window.onbeforeunload = function() { 
+       if(popit == true) {
+            popit = false;
+            return "Are you sure you want to leave?"; 
+       }
+ } 
+ */
