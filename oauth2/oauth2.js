bf14d71089c8ab6e5e65aab71f5190dc50254bcf
Il Kwon <ik226@cornell.edu>
2017-07-11 23:26:51 -0400

    added label modification for spam management

--- oauth2/oauth2.js
+++ oauth2/oauth2.js
@@ -8,6 +8,8 @@
 var querystring = require("querystring");
 var request = require('request');
 var constants = require('../constants.js');
+var converter = require('hex2dec');
+
 
 //TODO: reset keys n move sensitive info to external file
 // Keys n Constants
@@ -15,17 +17,28 @@ var GOOGLE_CLIENT_ID = '788164556802-77032h0shl056j1jkdpv58irspq6kavj.apps.googl
 var GOOGLE_CLIENT_SECRET = '-g2U1yx5rnajiQub9y5CJPc8'; //should be kept secret
 var BASE_URL = 'https://accounts.google.com/';
 
-//googleapis with oauth2client
+/*
+	googleapis with oauth2client
+
+*/
 var googleapis = require('googleapis');
 var OAuth2Client = googleapis.auth.OAuth2;
 var oauth2Client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, constants.REDIRECT_URI);
 
-//request new access token and callback
 exports.genNewAccessToken = function(token, callback){
+	
 	oauth2Client.credentials = token;
-	oauth2Client.refreshAccessToken(callback);
+	oauth2Client.refreshAccessToken(function(err, tokens){
+		oauth2Client.setCredentials(tokens);
+		//console.log(tokens);
+		callback(err, tokens);
+	});
 }
 
+exports.setCredentials = function(token){
+	oauth2Client.setCredentials(token);
+	
+}
 
 /* Generates a url that needs to be visited to get an authorization 
  * code. Redirect url is the url that will be visited after the 
@@ -36,14 +49,16 @@ exports.genNewAccessToken = function(token, callback){
 exports.genPermissionUrl = function (redirect_uri, login_hint){
     if (typeof redirect_uri != 'string'){
         console.log('Error:oauth.js:genPermissionUrl:Invalid Redirect Url')
-        return
+        return;
     }
     var parms = { 
         client_id: GOOGLE_CLIENT_ID, 
         redirect_uri: redirect_uri, 
         scope: 'https://mail.google.com/ https://www.googleapis.com/auth/userinfo.email',
-        approval_prompt: 'force',
-        access_type: 'offline',
+        //approval_prompt: 'force',
+        //access_type: 'offline',
+		approval_prompt: 'auto',
+		access_type: 'online',
         response_type: 'code',
         login_hint: login_hint || ''
         };
@@ -102,3 +117,63 @@ exports.buildXoauth2Token = function(email, access_token){
 };
 
 
+//Gmail GET specific email
+exports.getOneEmail = function(msgId, callback){
+	
+	var converted = converter.decToHex(msgId);
+	var hexId = converted.substring(2,converted.length);
+	
+	var gmail = googleapis.gmail({
+		version:'v1',
+		auth: oauth2Client
+	});
+	//convert msgId from decimal to hex
+	//var hexId = Number(msgId).toString(16);
+	
+	gmail.users.messages.get({
+		userId: 'me',
+		id: hexId
+	}, callback);
+	
+};
+//modify one email's label
+//TODO: make it batch modify
+
+//@param {list} msgId - IDs of email messages to be modified
+//@parma {string} newLabel - label id that email messages be attatched 
+exports.modifyEmailLabel = function(msgId, newLabel, callback){
+	
+	//convert msgId from decimal to hex
+	var idList = [];
+	msgId.forEach(function(d){
+		var converted = converter.decToHex(d);
+		var hexId = converted.substring(2,converted.length);
+		idList.push(hexId);
+	});
+	
+	
+	var gmail = googleapis.gmail({
+		version: 'v1',
+		auth: oauth2Client 
+	});
+	
+	gmail.users.messages.batchModify({
+		
+		userId: 'me',
+		resource: {
+			ids: idList,
+			addLabelIds: [newLabel],
+			removeLabelIds: []
+		}
+	}, callback);
+};
+
+//get User's label list
+//@param {string} email - user's email address
+exports.getUserEmailLabels = function(email, callback){
+	var gmail = googleapis.gmail('v1');
+	gmail.users.labels.list({
+		auth: oauth2Client,
+		userId: 'me'
+	}, callback);
+};
